ff1a0734332476cf2a2cad230f3d9c64fd5d3cfc
Steven <stevenren999@gmail.com>
2023-10-18 17:50:31 -0700

    End of Solo Development

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -1,6 +1,7 @@
 import * as Phaser from "phaser";
 
 import starfieldUrl from "/assets/starfield.png";
+import spaceshipUrl from "/assets/spaceship.png";
 
 export default class Play extends Phaser.Scene {
   fire?: Phaser.Input.Keyboard.Key;
@@ -9,8 +10,10 @@ export default class Play extends Phaser.Scene {
 
   starfield?: Phaser.GameObjects.TileSprite;
   spinner?: Phaser.GameObjects.Shape;
+  spaceShip?: Phaser.GameObjects.Sprite;
 
   rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
+  isFiring = false;
 
   constructor() {
     super("play");
@@ -18,6 +21,7 @@ export default class Play extends Phaser.Scene {
 
   preload() {
     this.load.image("starfield", starfieldUrl);
+    this.load.image("spaceship", spaceshipUrl);
   }
 
   #addKey(
@@ -40,27 +44,45 @@ export default class Play extends Phaser.Scene {
         "starfield",
       )
       .setOrigin(0, 0);
-
-    this.spinner = this.add.rectangle(100, 100, 50, 50, 0xff0000);
+    this.spinner = this.add.rectangle(100, 430, 50, 50, 0x67b36c);
+    this.spaceShip = this.add.sprite(640, 100, "spaceship");
   }
 
-  update(_timeMs: number, delta: number) {
+  // update(_timeMs: number, delta: number) {
+  update() {
     this.starfield!.tilePositionX -= 4;
 
     if (this.left!.isDown) {
-      this.spinner!.rotation -= delta * this.rotationSpeed;
+      //this.spinner!.rotation -= delta * this.rotationSpeed;
+      this.spinner!.x -= 3;
     }
     if (this.right!.isDown) {
-      this.spinner!.rotation += delta * this.rotationSpeed;
+      //this.spinner!.rotation += delta * this.rotationSpeed;
+      this.spinner!.x += 3;
     }
 
     if (this.fire!.isDown) {
+      /*
       this.tweens.add({
         targets: this.spinner,
         scale: { from: 1.5, to: 1 },
         duration: 300,
         ease: Phaser.Math.Easing.Sine.Out,
       });
+      */
+      this.isFiring = true;
+    }
+    if (this.isFiring) {
+      this.spinner!.y -= 3;
+    }
+    if (this.spinner!.y <= 0) {
+      this.spinner!.y = 430;
+      this.isFiring = false;
+    }
+
+    this.spaceShip!.x -= 3;
+    if (this.spaceShip!.x <= 0) {
+      this.spaceShip!.x = 640;
     }
   }
 }
